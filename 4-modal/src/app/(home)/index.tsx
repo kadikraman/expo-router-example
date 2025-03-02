2a4e310ea9e2c42b3000cb3e5e242f6de3d58019
Kadi Kraman <hellokadi@gmail.com>
2025-03-02 11:37:13 +0000

    Add alert

--- 4-modal/src/app/(home)/index.tsx
+++ 4-modal/src/app/(home)/index.tsx
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { Alert, View } from "react-native";
 import { AppText } from "@/components/AppText";
 import { Link, useRouter } from "expo-router";
 import { Button } from "@/components/Button";
@@ -6,6 +6,23 @@ import { Button } from "@/components/Button";
 export default function IndexScreen() {
   const router = useRouter();
   const canGoBack = router.canGoBack();
+
+  // https://reactnative.dev/docs/alert
+  const handleOpenAlert = () => {
+    Alert.alert("Warning!", "Are you sure you want to proceed?", [
+      {
+        text: "Cancel",
+        style: "cancel",
+      },
+      {
+        text: "Confirm",
+        style: "destructive",
+        onPress: () => {
+          console.log("Let's go!");
+        },
+      },
+    ]);
+  };
 
   return (
     <View className="justify-center flex-1 p-4">
@@ -22,6 +39,11 @@ export default function IndexScreen() {
           }}
         />
       ) : null}
+      <Button
+        title="Open Alert"
+        theme="secondary"
+        onPress={() => handleOpenAlert()}
+      />
     </View>
   );
 }
