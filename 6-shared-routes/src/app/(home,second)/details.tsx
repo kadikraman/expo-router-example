198c7b5c524429c848848c30943999eaab8105f7
Kadi Kraman <hellokadi@gmail.com>
2025-03-06 16:29:11 +0000

    Segment-based background

--- 6-shared-routes/src/app/(home,second)/details.tsx
+++ 6-shared-routes/src/app/(home,second)/details.tsx
@@ -1,12 +1,30 @@
 import { View } from "react-native";
 import { AppText } from "@/components/AppText";
+import { useSegments } from "expo-router";
+import { cn } from "@/utils/cn";
 
 export default function DetailsScreen() {
+  const segments = useSegments();
+
   return (
-    <View className="justify-center flex-1 p-4 bg-green-200">
+    <View
+      className={cn(
+        "justify-center flex-1 p-4",
+        segments[0] === "(home)" && "bg-blue-200",
+        segments[0] === "(second)" && "bg-pink-200",
+      )}
+    >
       <AppText center size="heading">
         Details Screen
       </AppText>
+      <AppText center bold>
+        Segments
+      </AppText>
+      {segments.map((segment, index) => (
+        <AppText center key={segment}>
+          {index}: {segment}
+        </AppText>
+      ))}
     </View>
   );
 }
